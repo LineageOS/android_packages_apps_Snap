aea7ecc0b787deb51991188bf13ba3ed0ed8f744
Steve Kondik <shade@chemlab.org>
2013-10-14 12:25:08 -0700

    camera: Add parameter debugging support
    Change-Id: Ifc1653f0046af169ba36d94796b4d9a3055c6798

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2807,6 +2807,9 @@ public class VideoModule implements CameraModule,
         int jpegQuality = CameraProfile.getJpegEncodingQualityParameter(mCameraId,
                 CameraProfile.QUALITY_HIGH);
         mParameters.setJpegQuality(jpegQuality);
+
+        CameraUtil.dumpParameters(mParameters);
+
         //Call Qcom related Camera Parameters
         qcomSetCameraParameters();
 
