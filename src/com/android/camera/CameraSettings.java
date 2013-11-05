9bdeb15d7db28bf3311bbe7ef2239fa0aa35cc6e
Michael Bestas <mikeioannina@gmail.com>
2013-11-05 16:43:50 +0200

    Camera: Cleanup hardware key handling
    * Fix power shutter behaviour in camcorder mode
    * Disable volume change while in panorama mode, volume buttons should only
    handle camera operations while in the camera
    * Add camera/power/volume key shutter handling in panorama mode
    
    On devices with a hardware camera key:
    * Disable power & volume shutter features
    * Volume buttons control the zoom
    
    On devices without a hardware camera key:
    * When power shutter is disabled, volume buttons control the shutter (like AOSP)
    * When power shutter is enabled, volume buttons control the zoom
    
    Change-Id: I141946d5b6f5aec8b1e9a9f99270d30295596079

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -980,6 +980,7 @@ public class CameraSettings {
         ListPreference cameraHdr = group.findPreference(KEY_CAMERA_HDR);
         ListPreference disMode = group.findPreference(KEY_DIS);
         ListPreference cameraHdrPlus = group.findPreference(KEY_CAMERA_HDR_PLUS);
+        ListPreference powerShutter = group.findPreference(KEY_POWER_SHUTTER);
         ListPreference videoHfrMode =
                 group.findPreference(KEY_VIDEO_HIGH_FRAME_RATE);
         ListPreference seeMoreMode = group.findPreference(KEY_SEE_MORE);
@@ -1090,6 +1091,9 @@ public class CameraSettings {
         if (cameraHdrPlus != null && (!ApiHelper.HAS_CAMERA_HDR_PLUS ||
                 !GcamHelper.hasGcamCapture() || isFrontCamera)) {
             removePreference(group, cameraHdrPlus.getKey());
+        }
+        if (powerShutter != null && CameraUtil.hasCameraKey()) {
+            removePreference(group, powerShutter.getKey());
         }
 
         if (PersistUtil.isSaveInSdEnabled()) {
