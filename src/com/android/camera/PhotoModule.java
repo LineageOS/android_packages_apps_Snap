2a9565943325791a3a10462e70e9f027b7bec50c
Steve Kondik <shade@chemlab.org>
2013-12-03 18:33:47 +0530

    camera: Keep touch focus intact during back-to-back ZSL shots
     * If ZSL is enabled, we should not resetTouchFocus between shots.
     * Instead, re-kick the timer for each shot. If I set a focus point,
       I want that point to stick while I take a succession of shots.
    
    Change-Id: I407bb1e248cc902355d0294f47b071cc8a32fac3

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1255,9 +1255,9 @@ public class PhotoModule
                     mCameraDevice.cancelAutoFocus();
                 }
             } else if (((mCameraState != LONGSHOT) && (mReceivedSnapNum == mBurstSnapNum))
-                        || isLongshotDone()){
+                        || isLongshotDone()) {
                 mUI.enableShutter(true);
-                mFocusManager.resetTouchFocus();
+                mFocusManager.restartTouchFocusTimer();
                 if (CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(mFocusManager.getFocusMode(false))
                         || CameraUtil.FOCUS_MODE_MW_CONTINUOUS_PICTURE.equals(mFocusManager.getFocusMode(false))) {
                     mCameraDevice.cancelAutoFocus();
