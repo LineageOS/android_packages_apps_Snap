b9afe5c37966869e29d996d981dacf7765195fca
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-12-28 13:47:57 +0700

    Camera: fix preview for landscape devices
    Landscape devices have broken preview because PORTRAIT mode is
    being abused to check for screen rotation. Use proper rotation
    check to fix.
    
    Ported from cm-10.2
    
    Change-Id: I75588cbce9843e180e4be7591f6a43bcec788506

--- src/com/android/camera/VideoUI.java
+++ src/com/android/camera/VideoUI.java
@@ -382,9 +382,7 @@ public class VideoUI implements PieRenderer.PieListener,
         } else {
             ratio = (float) height / width;
         }
-        if (mOrientationResize &&
-                mActivity.getResources().getConfiguration().orientation
-                != Configuration.ORIENTATION_PORTRAIT) {
+        if (mOrientationResize && CameraUtil.isScreenRotated(mActivity)) {
             ratio = 1 / ratio;
         }
 
@@ -727,9 +725,7 @@ public class VideoUI implements PieRenderer.PieListener,
     }
 
     public void setAspectRatio(double ratio) {
-        if (mOrientationResize &&
-                mActivity.getResources().getConfiguration().orientation
-                != Configuration.ORIENTATION_PORTRAIT) {
+        if (mOrientationResize && CameraUtil.isScreenRotated(mActivity)) {
             ratio = 1 / ratio;
         }
 
