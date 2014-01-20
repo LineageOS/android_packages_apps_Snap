27775b13a08d8fba2b4f4da6969156fd306cde90
jt1134 <jt1134@gmail.com>
2014-01-20 19:45:13 +0200

    Camera: separate settings for color effects
    Store color effect settings for the camera and
    video camera in their own separate preferences.
    
    Change-Id: I8faa59b8663154737910b338906d27f9736d5ad9

--- src/com/android/camera/VideoMenu.java
+++ src/com/android/camera/VideoMenu.java
@@ -506,7 +506,7 @@ public class VideoMenu extends MenuController
     public void initFilterModeButton(View button) {
         button.setVisibility(View.INVISIBLE);
         final IconListPreference pref = (IconListPreference) mPreferenceGroup
-                .findPreference(CameraSettings.KEY_COLOR_EFFECT);
+                .findPreference(CameraSettings.KEY_VIDEOCAMERA_COLOR_EFFECT);
         if (pref == null || pref.getValue() == null)
             return;
 
@@ -534,7 +534,7 @@ public class VideoMenu extends MenuController
 
     public void addFilterMode() {
         final IconListPreference pref = (IconListPreference) mPreferenceGroup
-                .findPreference(CameraSettings.KEY_COLOR_EFFECT);
+                .findPreference(CameraSettings.KEY_VIDEOCAMERA_COLOR_EFFECT);
         if (pref == null)
             return;
 
