36cc7d8fd0e8dd811dde338e812293e884b1a2bd
Michael Bestas <mikeioannina@gmail.com>
2014-10-14 06:21:58 +0300

    Camera2: Add option to set max screen brightness
    * Set maximum screen brightness while the user is inside the camera app
    
    Change-Id: I8b16ba47a933bc7d6b0c1cd62bfd6ca54875ce1e

--- src/com/android/camera/ComboPreferences.java
+++ src/com/android/camera/ComboPreferences.java
@@ -158,7 +158,8 @@ public class ComboPreferences implements
                 || key.equals(SettingsManager.KEY_MONO_ONLY)
                 || key.equals(SettingsManager.KEY_MONO_PREVIEW)
                 || key.equals(SettingsManager.KEY_CLEARSIGHT)
-                || key.equals(CameraSettings.KEY_POWER_SHUTTER);
+                || key.equals(CameraSettings.KEY_POWER_SHUTTER)
+                || key.equals(CameraSettings.KEY_MAX_BRIGHTNESS);
     }
 
     @Override
