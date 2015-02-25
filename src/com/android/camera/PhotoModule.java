5933a0b131eb0d53f3ae0fb06d3a76a8f7a6c6a2
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2015-02-25 18:23:58 +0530

    SnapdragonCamera: Fix AEC lock issue after non-zsl snapshot.
    In non-zsl snapshot, focus manager is set to unlock AEC after setting
    camera parameters in startpreview(). Due to this, AEC gets unlocked
    only after next touch focus or any set param call. Fixed this by
    changing the order during startpreview().
    
    Change-Id: I320d7b37c7107ce2242a52979505ff04029a83fa

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2771,6 +2771,10 @@ public class PhotoModule
             mFocusManager.setAeAwbLock(false); // Unlock AE and AWB.
         }
 
+        if (!mSnapshotOnIdle) {
+            mFocusManager.setAeAwbLock(false); // Unlock AE and AWB.
+        }
+
         setCameraParameters(UPDATE_PARAM_ALL);
 
         mCameraDevice.startPreview();
