24081ba61c4d799e6d18cc6543a49247e6aeb0a1
likaid <likaid@codeaurora.org>
2015-08-24 14:52:23 +0800

    SnapdragonCamera: Set color effect as none when turn HDR on
    Some color effects has special tone, if do HDR for specific color
    effects like Negative, we'll get unreasonable pictures.
    
    Disable the color effect when HDR on and set it as none effect.
    
    Change-Id: I8dca19b081504abaa85728b6d67b85699a184abb
    CRs-Fixed: 894360

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1832,6 +1832,17 @@ public class PhotoModule
             String whiteBalance = Parameters.WHITE_BALANCE_AUTO;
             focusMode = mFocusManager.getFocusMode();
             colorEffect = mParameters.getColorEffect();
+            String defaultEffect = mActivity.getString(R.string.pref_camera_coloreffect_default);
+            if (CameraUtil.SCENE_MODE_HDR.equals(mSceneMode)
+                    && colorEffect != null & !colorEffect.equals(defaultEffect)) {
+                disableLongShot = true;
+                // Change the colorEffect to default(None effect) when HDR ON.
+                colorEffect = defaultEffect;
+                mUI.setPreference(CameraSettings.KEY_COLOR_EFFECT, colorEffect);
+                mParameters.setColorEffect(colorEffect);
+                mCameraDevice.setParameters(mParameters);
+                mParameters = mCameraDevice.getParameters();
+            }
             exposureCompensation =
                 Integer.toString(mParameters.getExposureCompensation());
 
@@ -1843,9 +1854,6 @@ public class PhotoModule
                     getSharpnessSafe(),
                     colorEffect,
                     sceneMode, redeyeReduction, aeBracketing);
-            if (CameraUtil.SCENE_MODE_HDR.equals(mSceneMode)) {
-                disableLongShot = true;
-            }
         } else if (mFocusManager.isZslEnabled()) {
             focusMode = mParameters.getFocusMode();
             overrideCameraSettings(flashMode, null, focusMode,
