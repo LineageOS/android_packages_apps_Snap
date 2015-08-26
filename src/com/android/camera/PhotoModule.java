20a0405136a2d3fffe555ce4d07430170f08e301
likaid <likaid@codeaurora.org>
2015-08-26 10:24:05 +0800

    SnapdragonCamera: Fix not able to view other pictures when macro mode
    In Macro+ mode, it always started the refocus editor view when clicked
    the preview thumbnail, that caused user could not view other pictures.
    
    Reset the flag mLastPhotoTakenWithRefocus to false when onResume,start
    the refocus editor only one time after take a picture with macro+ mode.
    
    Change-Id: I07772772be0e6227b87c4f58c900a6c024cd2fdc
    CRs-Fixed: 896098

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2317,6 +2317,7 @@ public class PhotoModule
 
     @Override
     public void onResumeAfterSuper() {
+        mLastPhotoTakenWithRefocus = false;
         // Add delay on resume from lock screen only, in order to to speed up
         // the onResume --> onPause --> onResume cycle from lock screen.
         // Don't do always because letting go of thread can cause delay.
