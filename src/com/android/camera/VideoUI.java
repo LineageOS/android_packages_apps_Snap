d39f18084212bc11e845549156ad58da0ecef920
jumoog <github@jumoog.io>
2015-09-19 12:09:12 +0000

    Snap: Fix NPE when switching video effects and click on screen
    * Check if mMenuLayout is null, like in 23901e48 for PhotoUI

--- src/com/android/camera/VideoUI.java
+++ src/com/android/camera/VideoUI.java
@@ -694,8 +694,11 @@ public class VideoUI implements PieRenderer.PieListener,
     }
 
     public boolean sendTouchToMenu(MotionEvent ev) {
-        View v = mMenuLayout.getChildAt(0);
-        return v.dispatchTouchEvent(ev);
+        if (mMenuLayout != null) {
+            View v = mMenuLayout.getChildAt(0);
+            return v.dispatchTouchEvent(ev);
+        }
+        return false;
     }
 
     public void dismissSceneModeMenu() {
