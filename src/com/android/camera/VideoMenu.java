9cd569f3355bf1fd76cabf32961bc14341482473
Sultanxda <sultanxda@gmail.com>
2015-10-26 18:39:05 -0700

    SnapdragonCamera: Add option to control antibanding in camcorder
    The antibanding setting in camcorder is linked to the setting in camera
    mode for better UX (if you change one, the other will change accordingly).
    
    Change-Id: I98c1bc65472cf8eec530f5a7572f46458b0ab3fa

--- src/com/android/camera/VideoMenu.java
+++ src/com/android/camera/VideoMenu.java
@@ -141,6 +141,7 @@ public class VideoMenu extends MenuController
                 CameraSettings.KEY_VIDEO_ENCODER,
                 CameraSettings.KEY_AUDIO_ENCODER,
                 CameraSettings.KEY_VIDEO_HDR,
+                CameraSettings.KEY_ANTIBANDING,
                 CameraSettings.KEY_POWER_MODE,
                 CameraSettings.KEY_VIDEO_ROTATION,
                 CameraSettings.KEY_VIDEO_CDS_MODE,
