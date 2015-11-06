33bf2bd1c9aba40cf3eb3b470a10e31be82d28e2
Sultanxda <sultanxda@gmail.com>
2015-11-05 22:03:57 -0800

    SnapdragonCamera: Scale up bitrate for HSR recordings
    Currently, bitrate is only scaled up for HFR (slow-mo) recordings, not
    HSR (full high-speed) recordings.
    
    Scale up the bitrate for HSR to prevent video-quality degradation.
    
    Change-Id: Icc009ad4b22d990cc6df6773d965579a7d9cb091
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1577,7 +1577,8 @@ public class VideoModule implements CameraModule,
             }
             mMediaRecorder.setOutputFormat(mProfile.fileFormat);
             mMediaRecorder.setVideoFrameRate(mProfile.videoFrameRate);
-            mMediaRecorder.setVideoEncodingBitRate(mProfile.videoBitRate);
+            mMediaRecorder.setVideoEncodingBitRate(mProfile.videoBitRate *
+                                                ((isHSR ? captureRate : 30) / 30));
             mMediaRecorder.setVideoEncoder(mProfile.videoCodec);
             if (isHSR) {
                 Log.i(TAG, "Configuring audio for HSR");
