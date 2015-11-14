ee649f5811d8e0167c66ce5a8c69c7154f3dd05d
Sultanxda <sultanxda@gmail.com>
2015-11-13 21:32:34 -0800

    SnapdragonCamera: Unlock AE/AWB after taking a photo with ZSL
    The exposure and white balance get stuck after taking photos with ZSL.
    
    Always unlock the exposure and white balance after taking photos with
    ZSL.
    
    Change-Id: Ia34a54522048fdd619f44ac1f6d2924ce2c3fc6a

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2741,6 +2741,7 @@ public class PhotoModule
         if (null != mCameraDevice ) {
             mCameraDevice.cancelAutoFocus();
             setCameraState(IDLE);
+            mFocusManager.setAeAwbLock(false);
             setCameraParameters(UPDATE_PARAM_PREFERENCE);
         }
     }
