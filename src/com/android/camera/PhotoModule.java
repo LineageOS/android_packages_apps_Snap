3c6330160d5e8b7b9fe6baed0544cde0a42dc353
Sultanxda <sultanxda@gmail.com>
2015-12-12 21:59:51 -0800

    SnapdragonCamera: Set camera parameters before restarting preview
    Ensure that all of the updated camera parameters are propagated to the camera
    HAL prior to restarting the preview. This fixes the split-exposure observed when
    turning ZSL off and on without exiting the app.
    
    Change-Id: I392549ae7cc75b150e01cf8b635d2e3f1ad6ffb9
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -5037,6 +5037,7 @@ public class PhotoModule
         mLocationManager.recordLocation(recordLocation);
         if(needRestart()){
             Log.v(TAG, "Restarting Preview... Camera Mode Changed");
+            setCameraParameters(UPDATE_PARAM_PREFERENCE);
             stopPreview();
             startPreview();
             setCameraState(IDLE);
