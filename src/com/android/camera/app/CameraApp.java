185bfd4a342125b91cffe533f12917dd214f7d1e
codeworkx <codeworkx@cyanogenmod.org>
2016-01-05 20:34:03 +0100

    add support for lge-camera parameter
    Change-Id: Ib31557eb15c37bc928fa259bb098924f72f39da2

--- src/com/android/camera/app/CameraApp.java
+++ src/com/android/camera/app/CameraApp.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The Android Open Source Project
+ *               2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +18,25 @@
 package com.android.camera.app;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.android.camera.util.CameraUtil;
 import com.android.camera.util.UsageStatistics;
 
 public class CameraApp extends Application {
 
+    private static Application mApp = null;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mApp = this;
         UsageStatistics.initialize(this);
         CameraUtil.initialize(this);
     }
-}
 
+    public static Context getContext()
+    {
+        return mApp.getApplicationContext();
+    }
+}
