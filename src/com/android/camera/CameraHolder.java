e5594c27146de0bea0767a86d164a59bee0678fa
codeworkx <codeworkx@cyanogenmod.org>
2016-01-05 20:34:03 +0100

    add support for lge-camera parameter
    Change-Id: Ib31557eb15c37bc928fa259bb098924f72f39da2

--- src/com/android/camera/CameraHolder.java
+++ src/com/android/camera/CameraHolder.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ *               2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +19,7 @@ package com.android.camera;
 
 import static com.android.camera.util.CameraUtil.Assert;
 
+import android.content.Context;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Parameters;
 import android.os.Build;
@@ -26,7 +28,9 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
+import org.codeaurora.snapcam.R;
 
+import com.android.camera.app.CameraApp;
 import com.android.camera.CameraManager.CameraProxy;
 
 import java.io.IOException;
@@ -191,6 +195,9 @@ public class CameraHolder {
     public synchronized CameraProxy open(
             Handler handler, int cameraId,
             CameraManager.CameraOpenErrorCallback cb) {
+
+        Context context = CameraApp.getContext();
+
         if (DEBUG_OPEN_RELEASE) {
             collectState(cameraId, mCameraDevice);
             if (mCameraOpened) {
@@ -223,6 +230,15 @@ public class CameraHolder {
             }
             mCameraId = cameraId;
             mParameters = mCameraDevice.getCamera().getParameters();
+
+            // LGE Camera
+            final boolean lgeCamera = context.getResources().getBoolean(R.bool.lge_camera);
+            if (lgeCamera && mParameters != null) {
+                Log.d(TAG, "Set parameter lge-camera = 1");
+                mParameters.set("lge-camera", "1");
+                mCameraDevice.setParameters(mParameters);
+            }
+
         } else {
             if (!mCameraDevice.reconnect(handler, cb)) {
                 Log.e(TAG, "fail to reconnect Camera:" + mCameraId + ", aborting.");
