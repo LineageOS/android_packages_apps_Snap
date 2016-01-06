f80bf05652cb91a3c08a5d2c644f97ce1f2aa8fd
codeworkx <codeworkx@cyanogenmod.org>
2016-01-06 11:35:40 +0100

    Snap: Add support for shutter speed
    Author: codeworkx <codeworkx@cyanogenmod.org>
    Date:   Wed Jan 6 11:35:40 2016 +0100
    
        Snap: add support for shutter speed
    
        Change-Id: Id3393280c7c1c2f292d71587ba099e8dbafd1340
    
    Author: codeworkx <codeworkx@cyanogenmod.org>
    Date:   Fri Jan 8 19:38:15 2016 +0100
    
        Snap: Restart preview when shutter-speed gets disabled
    
        Change-Id: I3884b23f1c855cfffeb20ca6e2a760732370d772
    
    Change-Id: I82144c80d8fc2e7324b3dc8cde44b5b2934bdd50

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -3289,6 +3289,14 @@ public class PhotoModule
                 CameraSettings.setISOValue(mParameters, iso);
             }
         }
+        // Set shutter speed parameter
+        String shutterSpeed = mPreferences.getString(
+                CameraSettings.KEY_SHUTTER_SPEED,
+                mActivity.getString(R.string.pref_camera_shutter_speed_default));
+        if (CameraUtil.isSupported(shutterSpeed,
+            CameraSettings.getSupportedShutterSpeedValues(mParameters))) {
+            mParameters.set(CameraSettings.KEY_SNAPCAM_SHUTTER_SPEED, shutterSpeed);
+        }
         // Set color effect parameter.
         String colorEffect = mPreferences.getString(
                 CameraSettings.KEY_COLOR_EFFECT,
@@ -4147,6 +4155,19 @@ public class PhotoModule
         }
 
         mParameters.setJpegQuality(jpegQuality);
+
+        // When shutter speed gets disabled preview needs to be restarted
+        if (CameraUtil.isSupported(mParameters, CameraSettings.KEY_SNAPCAM_SHUTTER_SPEED)) {
+            String shutterSpeed = mPreferences.getString(CameraSettings.KEY_SHUTTER_SPEED, null);
+            if (shutterSpeed != null) {
+                String oldShutterSpeed = mParameters.get(CameraSettings.KEY_SNAPCAM_SHUTTER_SPEED);
+                if (!shutterSpeed.equals(oldShutterSpeed) && shutterSpeed.equals("0") &&
+                        mCameraState != PREVIEW_STOPPED) {
+                    Log.v(TAG, "Shutter speed disabled. Restart Preview");
+                    mRestartPreview = true;
+                }
+            }
+        }
 
         // For the following settings, we need to check if the settings are
         // still supported by latest driver, if not, ignore the settings.
