0a7b8bf5089c1cb5105cc8631aa6320eaaf4d4b6
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-07 21:07:51 +0100

    Snap: initial materialization
    Just for initial ship
    
    Change-Id: I8d6c0d71d1b94e6eb2f43ab962fb92de0e6c7093
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- src/com/android/camera/ui/OneUICameraControls.java
+++ src/com/android/camera/ui/OneUICameraControls.java
@@ -230,7 +230,7 @@ public class OneUICameraControls extends RotatableLayout {
 
         mViews = new View[]{
                 mSceneModeSwitcher, mFilterModeSwitcher, mFrontBackSwitcher,
-                mTsMakeupSwitcher, mFlashButton, mShutter, mPreview, mVideoShutter,
+                mTsMakeupSwitcher, mFlashButton, mPreview, mVideoShutter,
                 mPauseButton, mCancelButton
         };
         mBottomLargeSize = getResources().getDimensionPixelSize(
