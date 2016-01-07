496b0920015203dbdc616c869a2b970ca0c3caa7
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-07 21:07:51 +0100

    Snap: initial materialization
    Just for initial ship
    
    Change-Id: I8d6c0d71d1b94e6eb2f43ab962fb92de0e6c7093
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- src/com/android/camera/ShutterButton.java
+++ src/com/android/camera/ShutterButton.java
@@ -22,14 +22,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.android.camera.ui.RotateImageView;
-
 /**
  * A button designed to be used for the on-screen shutter button.
  * It's currently an {@code ImageView} that can call a delegate when the
  * pressed state changes.
  */
-public class ShutterButton extends RotateImageView {
+public class ShutterButton extends ImageView {
 
     private class LongClickListener implements View.OnLongClickListener {
          public boolean onLongClick(View v) {
@@ -82,51 +80,6 @@ public class ShutterButton extends RotateImageView {
     public void enableTouch(boolean enable) {
         mTouchEnabled = enable;
         setLongClickable(enable);
-    }
-
-    /**
-     * Hook into the drawable state changing to get changes to isPressed -- the
-     * onPressed listener doesn't always get called when the pressed state
-     * changes.
-     */
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-        final boolean pressed = isPressed();
-        if (pressed != mOldPressed) {
-            if (!pressed) {
-                // When pressing the physical camera button the sequence of
-                // events is:
-                //    focus pressed, optional camera pressed, focus released.
-                // We want to emulate this sequence of events with the shutter
-                // button. When clicking using a trackball button, the view
-                // system changes the drawable state before posting click
-                // notification, so the sequence of events is:
-                //    pressed(true), optional click, pressed(false)
-                // When clicking using touch events, the view system changes the
-                // drawable state after posting click notification, so the
-                // sequence of events is:
-                //    pressed(true), pressed(false), optional click
-                // Since we're emulating the physical camera button, we want to
-                // have the same order of events. So we want the optional click
-                // callback to be delivered before the pressed(false) callback.
-                //
-                // To do this, we delay the posting of the pressed(false) event
-                // slightly by pushing it on the event queue. This moves it
-                // after the optional click notification, so our client always
-                // sees events in this sequence:
-                //     pressed(true), optional click, pressed(false)
-                post(new Runnable() {
-                    @Override
-                    public void run() {
-                        callShutterButtonFocus(pressed);
-                    }
-                });
-            } else {
-                callShutterButtonFocus(pressed);
-            }
-            mOldPressed = pressed;
-        }
     }
 
     private void callShutterButtonFocus(boolean pressed) {
