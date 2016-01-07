29825cd2c1d4cab48fce847762ab95f7abf633d6
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-07 21:07:51 +0100

    Snap: initial materialization
    Just for initial ship
    
    Change-Id: I8d6c0d71d1b94e6eb2f43ab962fb92de0e6c7093
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1108,8 +1108,6 @@ public class PhotoModule
                     return;
                 }
 
-                mUI.doShutterAnimation();
-
                 Location loc = getLocationAccordPictureFormat(mParameters.get(KEY_PICTURE_FORMAT));
 
                 mLongShotCaptureCount++;
