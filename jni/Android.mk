ae92471b25d5d574158e8c3b0f4ab0b34497027f
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-01-09 05:23:08 +0200

    Snap: Fix jni compiler warnings
    Change-Id: I091a8928a9a0674c54759ff8b384259d8f16793e

--- jni/Android.mk
+++ jni/Android.mk
@@ -9,7 +9,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/feature_mos/src \
         $(LOCAL_PATH)/feature_mos/src/mosaic
 
-LOCAL_CFLAGS := -O3 -DNDEBUG -fstrict-aliasing
+LOCAL_CFLAGS := -O3 -DNDEBUG -fstrict-aliasing -Wno-unused-parameter
 
 LOCAL_SRC_FILES := \
         feature_mos_jni.cpp \
@@ -58,7 +58,7 @@ LOCAL_SDK_VERSION := 9
 LOCAL_MODULE    := libjni_snaptinyplanet
 LOCAL_SRC_FILES := tinyplanet.cc
 
-LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
+LOCAL_CFLAGS := -ffast-math -O3 -funroll-loops -Wno-unused-parameter
 LOCAL_ARM_MODE := arm
 
 include $(BUILD_SHARED_LIBRARY)
@@ -70,6 +70,6 @@ LOCAL_SDK_VERSION := 9
 LOCAL_MODULE    := libjni_imageutil
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := image_util_jni.cpp
-LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops
+LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops -Wno-unused-parameter
 include $(BUILD_SHARED_LIBRARY)
 
