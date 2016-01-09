d72580037b838e76ecf04e8429e79e6dc451ad6e
LuK1337 <priv.luk@gmail.com>
2016-01-09 13:08:19 +0100

    CameraActivity: Handle NPE when film strip view is null
    Change-Id: I0de4ec0f2cb4653822249ea97e8a07c890fc7231

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1955,7 +1955,9 @@ public class CameraActivity extends Activity
     }
 
     public void setPreviewGestures(PreviewGestures previewGestures) {
-        mFilmStripView.setPreviewGestures(previewGestures);
+        if(mFilmStripView != null) {
+            mFilmStripView.setPreviewGestures(previewGestures);
+        }
     }
 
     protected long updateStorageSpace() {
