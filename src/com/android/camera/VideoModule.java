2009e3043bbc6c7f6a5d228fae2889db935fd282
lion0738 <lion0738@naver.com>
2016-01-10 16:55:01 +0900

    Snap: Set parameters before starting preview
    Old parameters are used when grabbing focus mode.
    Camera parameters should be set to FocusOverlayManager before starting preview.
    
    Change-Id: I908559e1c2003be47b486996b0ec016b78107468

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -3086,18 +3086,19 @@ public class VideoModule implements CameraModule,
         mPreferences.setLocalId(mActivity, mCameraId);
         CameraSettings.upgradeLocalPreferences(mPreferences.getLocal());
         openCamera();
-        readVideoPreferences();
-        mUI.applySurfaceChange(VideoUI.SURFACE_STATUS.SURFACE_VIEW);
-        startPreview();
-        initializeVideoSnapshot();
-        resizeForPreviewAspectRatio();
-        initializeVideoControl();
 
         CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
         boolean mirror = (info.facing == CameraInfo.CAMERA_FACING_FRONT);
         mParameters = mCameraDevice.getParameters();
         mFocusManager.setMirror(mirror);
         mFocusManager.setParameters(mParameters);
+
+        readVideoPreferences();
+        mUI.applySurfaceChange(VideoUI.SURFACE_STATUS.SURFACE_VIEW);
+        startPreview();
+        initializeVideoSnapshot();
+        resizeForPreviewAspectRatio();
+        initializeVideoControl();
 
         initializeCapabilities();
 
