377973fbe84dbcec3dc331b15dd3bb9fa1dfe452
LuK1337 <priv.luk@gmail.com>
2016-01-11 17:40:58 +0100

    Move mApplicationContext to init()
    Fixes following NPE:
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: FATAL EXCEPTION: Thread-235
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: Process: org.cyanogenmod.snap, PID: 4329
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'android.content.res.Resources android.content.Context.getResources()' on a null object reference
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: 	at com.android.camera.PhotoModule.updateCameraParametersPreference(PhotoModule.java:3492)
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: 	at com.android.camera.PhotoModule.setCameraParameters(PhotoModule.java:3714)
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: 	at com.android.camera.PhotoModule.startPreview(PhotoModule.java:2715)
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: 	at com.android.camera.PhotoModule.-wrap14(PhotoModule.java)
    01-11 17:36:42.607  4329  4391 E AndroidRuntime: 	at com.android.camera.PhotoModule$OpenCameraThread.run(PhotoModule.java:265)
    
    Change-Id: I6ca0dfe402cab46dd7b6f102921062f3c7ec8d43

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -567,6 +567,7 @@ public class PhotoModule
         mCameraId = getPreferredCameraId(mPreferences);
 
         mContentResolver = mActivity.getContentResolver();
+        mApplicationContext = CameraApp.getContext();
 
         // Surface texture is from camera screen nail and startPreview needs it.
         // This must be done before startPreview.
@@ -903,8 +904,6 @@ public class PhotoModule
         if (mFirstTimeInitialized || mPaused) {
             return;
         }
-
-        mApplicationContext = CameraApp.getContext();
 
         // Initialize location service.
         boolean recordLocation = RecordLocationPreference.get(mPreferences);
