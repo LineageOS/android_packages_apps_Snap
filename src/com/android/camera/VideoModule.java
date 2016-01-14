db2718622223b230618e9dae5b0c5d2eb4e68741
Christopher R. Palmer <crpalmer@gmail.com>
2016-01-14 05:17:44 -0500

    Snap: Actually select the highest quality video by default
    Prior to this commit Snap assumes that the HAL returns the
    list of supported video sizes in descending order of quality and
    simply picks the first one from the list as the default quality.
    
    Instead, find the first matching entry of pref_video_quality_entryvalues
    that the HAL indicated was supported.
    
    Change-Id: Ifea79e0e16a9015557539e098317536a32b9ff1f

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -865,7 +865,7 @@ public class VideoModule implements CameraModule,
             } else {
                 // check for highest quality supported
                 videoQuality = CameraSettings.getSupportedHighestVideoQuality(
-                        mCameraId, mParameters);
+                        mActivity, mCameraId, mParameters);
             }
             mPreferences.edit().putString(CameraSettings.KEY_VIDEO_QUALITY, videoQuality).apply();
         }
