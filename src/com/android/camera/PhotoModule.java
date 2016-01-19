56280c578fab4c9a470e593dcf565bfce39bc6f8
Gabriele M <moto.falcon.git@gmail.com>
2016-01-19 16:45:25 +0100

    Snap: Don't enable ZSL when disabling HDR
    Instead, restore the default value. This prevents issues on devices
    that don't support ZSL.
    
    Change-Id: I9c456cd6503df29800e2bc7ccaa691cb3fb42681

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -4829,6 +4829,7 @@ public class PhotoModule
         //filter off unsupported settings
         final String settingOff = mActivity.getString(R.string.setting_off_value);
         final String settingOn = mActivity.getString(R.string.setting_on_value);
+        final String zsl = mActivity.getString(R.string.pref_camera_zsl_default);
         if (!CameraSettings.isZSLHDRSupported(mParameters)) {
             //HDR internally uses AE-bracketing. Disable both if not supported.
             if (notSame(pref, CameraSettings.KEY_CAMERA_HDR, settingOff) ||
@@ -4839,7 +4840,7 @@ public class PhotoModule
                 mUI.setPreference(CameraSettings.KEY_AE_BRACKET_HDR, settingOff);
             } else if (notSame(pref, CameraSettings.KEY_CAMERA_HDR, settingOn) ||
                 notSame(pref, CameraSettings.KEY_AE_BRACKET_HDR, settingOn)) {
-                mUI.setPreference(CameraSettings.KEY_ZSL,settingOn);
+                mUI.setPreference(CameraSettings.KEY_ZSL, zsl);
             }
         }
 
