cf30e0bb165558c41cee7c4946ceffdd1f5aead7
codeworkx <codeworkx@cyanogenmod.org>
2016-01-22 17:17:53 +0100

    Snap: Do not restart preview during longshots
    Change-Id: I6ad439dc0be7c6e0d13ac7c22aacdc0ad559d051

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1357,7 +1357,7 @@ public class PhotoModule
                 mParameters = mCameraDevice.getParameters();
                 mBurstSnapNum = CameraUtil.getNumSnapsPerShutter(mParameters);
             }
-            Log.v(TAG, "JpegPictureCallback: Received = " + mReceivedSnapNum +
+            Log.v(TAG, "JpegPictureCallback: Received = " + mReceivedSnapNum + " " +
                       "Burst count = " + mBurstSnapNum);
             // If postview callback has arrived, the captured image is displayed
             // in postview callback. If not, the captured image is displayed in
@@ -1384,7 +1384,7 @@ public class PhotoModule
                     && (mSnapshotMode != CameraInfoWrapper.CAMERA_SUPPORT_MODE_ZSL)
                     && (mReceivedSnapNum == mBurstSnapNum);
 
-            needRestartPreview |= mLgeHdrMode;
+            needRestartPreview |= mLgeHdrMode && (mCameraState != LONGSHOT);
 
             CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
 
@@ -1399,12 +1399,14 @@ public class PhotoModule
                         .getBoolean(R.bool.front_camera_restart_preview_onPictureTaken);
             }
 
-            if (isBackCamera && backCameraRestartPreviewOnPictureTaken ||
-                    isFrontCamera && frontCameraRestartPreviewOnPictureTaken) {
+            if ((isBackCamera && backCameraRestartPreviewOnPictureTaken ||
+                    isFrontCamera && frontCameraRestartPreviewOnPictureTaken) &&
+                    mCameraState != LONGSHOT) {
                 needRestartPreview = true;
             }
 
             if (needRestartPreview) {
+                Log.d(TAG, "JpegPictureCallback: needRestartPreview");
                 setupPreview();
                 focusMode = mFocusManager.getFocusMode();
                 if (CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(focusMode) ||
