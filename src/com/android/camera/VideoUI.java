808749224946d064badd0964a63d1a7a9086b952
Jack Yoo <jyoo@codeaurora.org>
2016-01-27 17:09:15 -0800

    SnapdragonCamera: Fix the memory leak on view.
    Keep using modules instead of creating.
    Don't remove the view without cleaning it.
    
    Change-Id: I260a660cac3d770f7d02dfdc1881e15490da66a0
    CRs-Fixed: 958510

--- src/com/android/camera/VideoUI.java
+++ src/com/android/camera/VideoUI.java
@@ -331,7 +331,7 @@ public class VideoUI implements PieRenderer.PieListener,
         }
 
         mCameraControls.setPreviewRatio(mAspectRatio, false);
-        layoutPreview((float)ratio);
+        layoutPreview((float) ratio);
     }
 
     private void layoutPreview(float ratio) {
@@ -485,6 +485,10 @@ public class VideoUI implements PieRenderer.PieListener,
 
     public void showSwitcher() {
         mSwitcher.setVisibility(View.VISIBLE);
+    }
+
+    public void setSwitcherIndex() {
+        mSwitcher.setCurrentIndex(ModuleSwitcher.VIDEO_MODULE_INDEX);
     }
 
     public boolean collapseCameraControls() {
