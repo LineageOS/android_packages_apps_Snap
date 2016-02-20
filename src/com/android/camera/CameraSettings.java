726776f56eee0e53321535a287ec2aa4bb3f9090
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-20 23:58:13 +0200

    Snap: Remove auto HDR option when not supported
    Change-Id: Iaff1bee5ad6d48df2efe189955ba91bca8ec5c32

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -897,7 +897,7 @@ public class CameraSettings {
             filterSimilarPictureSize(group, videoSnapSize);
         }
 
-        if (histogram!= null) {
+        if (histogram != null) {
             filterUnsupportedOptions(group,
                     histogram, mParameters.getSupportedHistogramModes());
         }
@@ -907,12 +907,17 @@ public class CameraSettings {
                     pictureFormat, getSupportedPictureFormatLists());
         }
 
-        if(advancedFeatures != null) {
+        if (advancedFeatures != null) {
             filterUnsupportedOptions(group,
                     advancedFeatures, getSupportedAdvancedFeatures(mParameters));
         }
-        if (longShot!= null && !isLongshotSupported(mParameters)) {
+
+        if (longShot != null && !isLongshotSupported(mParameters)) {
             removePreference(group, longShot.getKey());
+        }
+
+        if (auto_hdr != null && !CameraUtil.isAutoHDRSupported(mParameters)) {
+            removePreference(group, auto_hdr.getKey());
         }
 
         if (videoRotation != null) {
