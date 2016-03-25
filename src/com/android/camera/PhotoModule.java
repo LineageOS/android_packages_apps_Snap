143f1c6f0ee779b34e2bdd817b82c016080dab8f
Danny Baumann <dannybaumann@web.de>
2016-03-25 13:02:27 +0100

    Protect against multiple shutter callbacks per frame in longshot mode.
    Some devices send multiple shutter callbacks for one captured frame in
    longshot mode. As this causes mismatch between the calculated number of
    captured frames and the calculated number of JPEGs received for those,
    which in turn leads to longshot mode never being ended properly,
    protect against that scenario by discarding all but the first callback.
    
    Change-Id: I0c3a270f664a0cd16354fb0a13c5941a52001650

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1056,6 +1056,11 @@ public class PhotoModule
 
     private final class LongshotShutterCallback
             implements CameraShutterCallback {
+        private int mExpectedLongshotSnapNum;
+
+        public LongshotShutterCallback() {
+            mExpectedLongshotSnapNum = mLongshotSnapNum;
+        }
 
         @Override
         public void onShutter(CameraProxy camera) {
@@ -1063,6 +1068,9 @@ public class PhotoModule
             mShutterLag = mShutterCallbackTime - mCaptureStartTime;
             Log.e(TAG, "[KPI Perf] PROFILE_SHUTTER_LAG mShutterLag = " + mShutterLag + "ms");
             synchronized(mCameraDevice) {
+                if (mExpectedLongshotSnapNum != mLongshotSnapNum) {
+                    return;
+                }
 
                 if (++mLongshotSnapNum >= mLongShotMaxSnap &&
                     (mLongShotMaxSnap != -1)) {
