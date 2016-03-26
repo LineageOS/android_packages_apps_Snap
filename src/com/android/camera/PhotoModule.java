b899d39e88769873a52a7582bde72d295696fa2e
Sultanxda <sultanxda@gmail.com>
2016-03-26 04:45:50 -0700

    Snap: Don't do touch-to-focus on top of UI elements
    Don't process touch-to-focus for taps that are located on either the top
    or bottom UI panes.
    
    Change-Id: Iac4ccda8625080ff0bd1133ac339e53c423b626d
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2821,7 +2821,7 @@ public class PhotoModule
                 if (mFocusManager == null) {
                     mFocusManager = new FocusOverlayManager(mPreferences, defaultFocusModes,
                             mInitialParams, this, mMirror,
-                            mActivity.getMainLooper(), mUI);
+                            mActivity.getMainLooper(), mUI, mActivity);
                 }
             }
         }
