4b147067ccdcc92171b443ffbd53600d49335143
codeworkx <codeworkx@cyanogenmod.org>
2016-05-01 12:19:41 +0200

    Snap: Extend user menu, disable dev menu
    Change-Id: Iaa64372e5d196fd0117a32a386686bdb1fd51e45

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -388,6 +388,10 @@ public class CameraActivity extends Activity
         mDeveloperMenuEnabled = true;
     }
 
+    public void disableDeveloperMenu() {
+        mDeveloperMenuEnabled = false;
+    }
+
     public static void resetFirstStartAfterScreenOn() {
         sFirstStartAfterScreenOn = false;
     }
