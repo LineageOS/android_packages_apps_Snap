b53d909bb00c64332fe7e98406d2c10971523161
Adnan Begovic <adnan@cyngn.com>
2016-05-20 13:06:35 -0700

    Snap: Use new window manager flag for prevent system keys.
    Change-Id: I94ed12ddd33729a4fb486f7fb68f1bbab788b239
    TICKET: FEIJ-522

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1813,10 +1813,10 @@ public class CameraActivity extends Activity
         }
         if (mPowerShutter && mInCameraApp) {
             getWindow().addPrivateFlags(
-                    WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY);
+                    WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS);
         } else {
             getWindow().clearPrivateFlags(
-                    WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY);
+                    WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS);
         }
     }
 
