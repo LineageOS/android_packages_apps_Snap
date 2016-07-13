2cad43d610db287deec3b4c368e92901222ae8d9
Jack Yoo <jyoo@codeaurora.org>
2016-07-13 12:56:36 -0700

    SnapdragonCamera: Adding PRIVILEGED true
    Promoting privilege for direct SD card path access.
    
    Change-Id: I10f28606a646a85c0cb23e4cf8e617eafdcaa2b4
    CRs-Fixed: 1032564

--- Android.mk
+++ Android.mk
@@ -22,6 +22,7 @@ LOCAL_AAPT_FLAGS := \
         --version-code $(version_code_package) \
 
 LOCAL_PACKAGE_NAME := Snap
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_AAPT_FLAGS += --rename-manifest-package org.cyanogenmod.snap
 
