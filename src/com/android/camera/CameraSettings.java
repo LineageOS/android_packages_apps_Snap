68490764508182649f9d8a0535b21e04f00e46a1
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-07-18 18:10:08 +0800

    Snap: Fix incorrect default ISO values parameter
    This is a typo introduced by commit:
    c544747c2892b3984357a86b4116e1faa2be2210
    
    We intend to default the ISO values parameter to "iso-values",
    but we are currently setting the ISO parameter instead.
    
    Fix the typo.
    
    Change-Id: Icea4771767c04b08e2b888bc8fb55a05fc69fa91

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -336,7 +336,7 @@ public class CameraSettings {
         }
 
         if (mKeyIsoValues == null || mKeyIsoValues.isEmpty()) {
-            mKeyIso = "iso-values";
+            mKeyIsoValues = "iso-values";
         } else {
             Log.d(TAG, "Using key for iso-values: " + mKeyIsoValues);
         }
