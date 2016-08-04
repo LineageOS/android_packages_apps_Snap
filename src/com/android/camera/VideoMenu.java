2a587f13d0b5b800771ce74e5f1d0bdc11f48503
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-04 21:28:53 +0300

    Snap: Make developer menu more accessible
     * Not all devices have red eye reduction, which made it
       impossible to enable advanced options. Move the toggle
       to max brightness option which is present in all devices.
     * Allow enabling advanced options through camcorder settings.
     * Move hardcoded strings to cm_strings for translations.
     * Close menus when developer mode is toggled so we can see
       the extra options when the menu is reopened.
     * Decrease taps to 7, matching Settings tap-to-enable.
     * Properly reset tap counter.
    
    Change-Id: Iac39b9309388b92bf75a49a6091b483b13bd9154

--- src/com/android/camera/VideoMenu.java
+++ src/com/android/camera/VideoMenu.java
@@ -78,6 +78,7 @@ public class VideoMenu extends MenuController
     private static final int PREVIEW_MENU_IN_ANIMATION = 1;
     private static final int PREVIEW_MENU_ON = 2;
     private static final int MODE_FILTER = 1;
+    private static final int DEVELOPER_MENU_TOUCH_COUNT = 7;
     private int mSceneStatus;
     private View mFrontBackSwitcher;
     private View mFilterModeSwitcher;
@@ -87,6 +88,7 @@ public class VideoMenu extends MenuController
     private String mPrevSavedVideoCDS;
     private boolean mIsVideoTNREnabled = false;
     private boolean mIsVideoCDSUpdated = false;
+    private int mPrivateCounter = 0;
     private static final int ANIMATION_DURATION = 300;
     private static final int CLICK_THRESHOLD = 200;
     private int previewMenuSize;
@@ -893,6 +895,31 @@ public class VideoMenu extends MenuController
             mUI.showPopup(mListSubMenu, 2, true);
         }
         mPopupStatus = POPUP_SECOND_LEVEL;
+
+        // Developer menu
+        if (pref.getKey().equals(CameraSettings.KEY_MAX_BRIGHTNESS)) {
+            mPrivateCounter++;
+            if (mPrivateCounter >= DEVELOPER_MENU_TOUCH_COUNT) {
+                SharedPreferences prefs = PreferenceManager
+                        .getDefaultSharedPreferences(mActivity);
+                if (!mActivity.isDeveloperMenuEnabled()) {
+                    mActivity.enableDeveloperMenu();
+                    prefs.edit().putBoolean(CameraSettings.KEY_DEVELOPER_MENU, true).apply();
+                    closeAllView();
+                    RotateTextToast.makeText(mActivity,
+                            R.string.developer_menu_enabled, Toast.LENGTH_SHORT).show();
+                } else {
+                    mActivity.disableDeveloperMenu();
+                    prefs.edit().putBoolean(CameraSettings.KEY_DEVELOPER_MENU, false).apply();
+                    closeAllView();
+                    RotateTextToast.makeText(mActivity,
+                            R.string.developer_menu_disabled, Toast.LENGTH_SHORT).show();
+                }
+                mPrivateCounter = 0;
+            }
+        } else {
+            mPrivateCounter = 0;
+        }
     }
 
     public void onListMenuTouched() {
