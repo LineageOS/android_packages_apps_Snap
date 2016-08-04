51be6e84c5d31d66f89bcc528e30f025133a71f6
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-04 07:23:05 -0700

    Revert "SnapdragonCamera: Fix AEC lock issue after non-zsl snapshot."
    * Duplicate commit
    
    This reverts commit 5933a0b131eb0d53f3ae0fb06d3a76a8f7a6c6a2.
    
    Change-Id: Ib60d5c9dd463c007a3cffdc9be7897539713cd3d

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2844,10 +2844,6 @@ public class PhotoModule
             mFocusManager.setAeAwbLock(false); // Unlock AE and AWB.
         }
 
-        if (!mSnapshotOnIdle) {
-            mFocusManager.setAeAwbLock(false); // Unlock AE and AWB.
-        }
-
         setCameraParameters(UPDATE_PARAM_ALL);
 
         mCameraDevice.startPreview();
