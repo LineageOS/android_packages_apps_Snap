f7042ee46f040165175347503b5a94769a5c4733
darkobas <darkobas@gmail.com>
2016-08-09 00:13:41 +0200

    SnapdragonCamera: Panorama, replace border drawable
    Change-Id: Ic86c7146f53b96e70259fd6da81050277ebda23b

--- src/com/android/camera/WideAnglePanoramaUI.java
+++ src/com/android/camera/WideAnglePanoramaUI.java
@@ -393,9 +393,6 @@ public class WideAnglePanoramaUI implements
         mTextureView.setLayoutParams(param);
         mTextureView.setX(xOffset);
         mTextureView.setY(yOffset);
-        mPreviewBorder.setLayoutParams(param);
-        mPreviewBorder.setX(xOffset);
-        mPreviewBorder.setY(yOffset);
         mPreviewYOffset = yOffset;
 
         mTextureView.layout(0, mPreviewYOffset, mTextureView.getRight(), mTextureView.getBottom());
@@ -711,6 +708,7 @@ public class WideAnglePanoramaUI implements
         mPanoFailedDialog.setRotation(-orientation);
         mReview.setRotation(-orientation);
         mTooFastPrompt.setRotation(-orientation);
+        mPreviewBorder.setRotation(-orientation);
         mCameraControls.setOrientation(orientation, animation);
         RotateTextToast.setOrientation(orientation);
     }
