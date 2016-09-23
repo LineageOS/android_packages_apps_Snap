cc08b441ef6e94b95babc9f93420798bc6f52311
Jack Yoo <jyoo@codeaurora.org>
2016-09-23 11:18:15 -0700

    SnapdragonCamera: Camera1 Selfie Mirror orientation check
    Checking the case that front facing camera is 90 instead of 270.
    
    Change-Id: I75319db771057d64d1a21a1de825edd54af83982
    CRs-Fixed: 1072060

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1262,10 +1262,14 @@ public class PhotoModule extends BaseModule<PhotoUI> implements
         }
     }
 
-    private byte[] flipJpeg(byte[] jpegData) {
+    private byte[] flipJpeg(byte[] jpegData, int orientation) {
         Bitmap srcBitmap = BitmapFactory.decodeByteArray(jpegData, 0, jpegData.length);
         Matrix m = new Matrix();
-        m.preScale(-1, 1);
+        if(orientation == 270) {
+            m.preScale(-1, 1);
+        } else { //if it's 90
+            m.preScale(1, -1);
+        }
         Bitmap dstBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(), srcBitmap.getHeight(), m, false);
         dstBitmap.setDensity(DisplayMetrics.DENSITY_DEFAULT);
         int size = dstBitmap.getWidth() * dstBitmap.getHeight();
@@ -1273,6 +1277,18 @@ public class PhotoModule extends BaseModule<PhotoUI> implements
         dstBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
 
         return outStream.toByteArray();
+    }
+
+    public static byte[] addExifTags(byte[] jpeg, int orientationInDegree) {
+        ExifInterface exif = new ExifInterface();
+        exif.addOrientationTag(orientationInDegree);
+        ByteArrayOutputStream jpegOut = new ByteArrayOutputStream();
+        try {
+            exif.writeExif(jpeg, jpegOut);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write EXIF", e);
+        }
+        return jpegOut.toByteArray();
     }
 
     private final class JpegPictureCallback
@@ -1415,9 +1431,8 @@ public class PhotoModule extends BaseModule<PhotoUI> implements
                             .findPreference(CameraSettings.KEY_SELFIE_MIRROR);
                     if (selfieMirrorPref != null && selfieMirrorPref.getValue() != null &&
                             selfieMirrorPref.getValue().equalsIgnoreCase("enable")) {
-                        jpegData = flipJpeg(jpegData);
-                        exif = Exif.getExif(jpegData);
-                        exif.addOrientationTag(orientation);
+                        jpegData = flipJpeg(jpegData, info.orientation);
+                        jpegData = addExifTags(jpegData, orientation);
                     }
                 }
                 if (!mIsImageCaptureIntent) {
