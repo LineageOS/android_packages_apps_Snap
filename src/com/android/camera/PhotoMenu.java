a4a4bda1de280a4517f742fb9051473e546cbc08
Christian Frisson <christian.frisson@gmail.com>
2016-10-15 22:07:17 +0200

    Snap: Allow switching beyond just 2 cameras
    Some devices have dual rear cameras, which means that they will have
    more than just 2 camera ID's (0 and 1). For example, the LG G5 has
    0, 1 and 2. Allow Snap to run cycle through more than just 0 and 1
    
    Change-Id: I50d8b6c02c97961990b1fbf69b9cf706c422381c

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -825,6 +825,7 @@ public class PhotoMenu extends MenuController
         int index = pref.findIndexOfValue(pref.getValue());
         if (!pref.getUseSingleIcon() && iconIds != null) {
             // Each entry has a corresponding icon.
+            index = index % iconIds.length;
             resid = iconIds[index];
         } else {
             // The preference only has a single icon to represent it.
@@ -851,8 +852,9 @@ public class PhotoMenu extends MenuController
                 CharSequence[] values = pref.getEntryValues();
                 index = (index + 1) % values.length;
                 pref.setValueIndex(index);
+                int iconListLength = ((IconListPreference) pref).getLargeIconIds().length;
                 ((ImageView) v).setImageResource(
-                        ((IconListPreference) pref).getLargeIconIds()[index]);
+                        ((IconListPreference) pref).getLargeIconIds()[index % iconListLength]);
                 if (prefKey.equals(CameraSettings.KEY_CAMERA_ID))
                     mListener.onCameraPickerClicked(index);
                 reloadPreference(pref);
