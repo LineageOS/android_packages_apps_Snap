fab321f61b6fe4e1296806ff5b04bac19d1e4de6
weijiew <weijiew@codeaurora.org>
2016-11-17 10:15:15 +0800

    SnapdragonCamera: Camera switches from bayer and mono on switching to front camera
    Dual camera was not supported, camera was switched one by one.
    Solution:
    only select bayer when user switch front camera to rear camera
    
    Change-Id: I814552aed1ee8889f309cb5de8df84f9bee42093
    CRs-Fixed: 1088821

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -1017,6 +1017,10 @@ public class CameraSettings {
             return;
         }
 
+        if (numOfCameras > 2 ) {
+            numOfCameras = 2;
+        }
+
         CharSequence[] entryValues = new CharSequence[numOfCameras];
         for (int i = 0; i < numOfCameras; ++i) {
             entryValues[i] = "" + i;
