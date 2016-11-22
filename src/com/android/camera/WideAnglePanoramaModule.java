2081881f18a159001edca328a23a8d9d2c762dc6
Steve Kondik <steve@cyngn.com>
2016-11-22 01:57:02 +0100

    snap: Panorama fixes
    Change-Id: I9474219c57fc204dd16797aa8921296e61cc9d37

--- src/com/android/camera/WideAnglePanoramaModule.java
+++ src/com/android/camera/WideAnglePanoramaModule.java
@@ -689,7 +689,6 @@ public class WideAnglePanoramaModule
     }
 
     public void reportProgress() {
-        mUI.showUI();
         mUI.resetSavingProgress();
         Thread t = new Thread() {
             @Override
@@ -1143,6 +1142,13 @@ public class WideAnglePanoramaModule
             mCameraTexture.setOnFrameAvailableListener(this);
             mCameraDevice.setPreviewTexture(mCameraTexture);
         }
+        mCameraDevice.setOneShotPreviewCallback(mMainHandler,
+                new CameraManager.CameraPreviewDataCallback() {
+                    @Override
+                    public void onPreviewFrame(byte[] data, CameraProxy camera) {
+                        mUI.hidePreviewCover();
+                    }
+                });
         mCameraDevice.startPreview();
         mCameraState = PREVIEW_ACTIVE;
     }
