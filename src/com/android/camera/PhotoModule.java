86e56a6d493feba566f51269266197ed820bac98
Danny Baumann <dannybaumann@web.de>
2016-11-30 10:40:29 +0100

    Initialize focus manager in onResume().
    Since commit 87b4a40d588a7ae8a61cb353c4cff8df37f7b483 moved camera
    initialization (and focus manager initialization with it) to a
    background thread, the focus manager initialization now races against
    the onPreviewRectChanged() call triggered by SurfaceView initialization
    in the main thread, potentially leaving the focus manager in an
    uninitialized state that prevented taking pictures.
    
    Change-Id: I8cf650d6a67768acd131b3cddad175ed198e0838

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2691,6 +2691,7 @@ public class PhotoModule
     @Override
     public void onResumeBeforeSuper() {
         mPaused = false;
+        if (mFocusManager == null) initializeFocusManager();
     }
 
     private void openCamera() {
--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1231,6 +1231,7 @@ public class VideoModule implements CameraModule,
     @Override
     public void onResumeBeforeSuper() {
         mPaused = false;
+        if (mFocusManager == null) initializeFocusManager();
     }
 
     @Override
