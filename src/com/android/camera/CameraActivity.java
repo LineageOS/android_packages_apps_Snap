6a1e5f5f2b47adb06a890d271872557a6df2cdb9
Joey Rizzoli <joey@cyanogenmoditalia.it>
2016-12-11 20:35:55 +0100

    Snap: prevent NPE when checking if controls are visible
    Change-Id: Id054ccc9445722bcb08953fd40e43a82bdd13ae2
    Signed-off-by: Joey Rizzoli <joey@cyanogenmoditalia.it>

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -2432,6 +2432,9 @@ public class CameraActivity extends Activity
      * @return whether controls are visible.
      */
     private boolean arePreviewControlsVisible() {
+        if (mCurrentModule == null) {
+            return false;
+        }
         return mCurrentModule.arePreviewControlsVisible();
     }
 
