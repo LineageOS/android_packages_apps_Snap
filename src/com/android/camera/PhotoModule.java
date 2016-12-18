99fcf55ccadda6455e9d0384aa546829cccafd9a
Sultanxda <sultanxda@gmail.com>
2016-12-18 10:58:42 -0800

    SnapdragonCamera: Enforce 120ms delay in between shutter clicks
    To prevent unintentional duplicate photo captures.
    
    Change-Id: I5b24d834ba6eb7e2f606d656a71f5878ab8d08f5
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -159,6 +159,7 @@ public class PhotoModule
     private static final int SET_PHOTO_UI_PARAMS = 11;
     private static final int SWITCH_TO_GCAM_MODULE = 12;
     private static final int ON_PREVIEW_STARTED = 13;
+    private static final int UNLOCK_CAM_SHUTTER = 14;
 
     // The subset of parameters we need to update in setCameraParameters().
     private static final int UPDATE_PARAM_INITIALIZE = 1;
@@ -545,6 +546,11 @@ public class PhotoModule
 
                 case ON_PREVIEW_STARTED: {
                     onPreviewStarted();
+                    break;
+                }
+
+                case UNLOCK_CAM_SHUTTER: {
+                    mUI.enableShutter(true);
                     break;
                 }
             }
@@ -1817,10 +1823,12 @@ public class PhotoModule
         mPreviewRestartSupport &= PIXEL_FORMAT_JPEG.equalsIgnoreCase(
                 pictureFormat);
 
+        mUI.enableShutter(false);
+
         // We don't want user to press the button again while taking a
         // multi-second HDR photo. For longshot, no need to disable.
-        if (CameraUtil.SCENE_MODE_HDR.equals(mSceneMode)) {
-            mUI.enableShutter(false);
+        if (!CameraUtil.SCENE_MODE_HDR.equals(mSceneMode)) {
+            mHandler.sendEmptyMessageDelayed(UNLOCK_CAM_SHUTTER, 120);
         }
 
         if (!isShutterSoundOn()) {
