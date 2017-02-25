48f0e427c4d60f597235954d9bc910c5c8980dbe
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-02-25 12:49:16 +0100

    Snap: CaptureModule: lock exposure also when precapture trigger is started
    Fixes camera hang on LG G4 when flash mode is auto and flash is required.
    
    Change-Id: I258fed2364e37f6b40ee5e13f3359167ce13ff8f

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -574,10 +574,13 @@ public class CaptureModule extends BaseModule<CaptureUI> implements PhotoControl
                 Log.d(TAG, "STATE_WAITING_PRECAPTURE id: " + id + " afState: " + afState + " aeState:" + aeState);
                 if (aeState == null ||
                         aeState == CaptureResult.CONTROL_AE_STATE_PRECAPTURE ||
+                        aeState == CaptureResult.CONTROL_AE_PRECAPTURE_TRIGGER_START ||
                         aeState == CaptureResult.CONTROL_AE_STATE_FLASH_REQUIRED ||
                         aeState == CaptureResult.CONTROL_AE_STATE_CONVERGED) {
-                    if (mPrecaptureRequestHashCode[id] == result.getRequest().hashCode())
+                    if (mPrecaptureRequestHashCode[id] == result.getRequest().hashCode()) {
+                        Log.d(TAG, "updateCaptureStateMachine: hashes are equal, lock exposure");
                         lockExposure(id);
+                    }
                 }
                 break;
             }
