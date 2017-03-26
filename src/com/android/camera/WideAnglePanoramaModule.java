7882513b554bfcbbb0dc47d6ca3ff3cb9cf9d906
Gabriele M <moto.falcon.git@gmail.com>
2017-03-26 12:13:09 +0200

    Always apply frame size reduction to panorama pictures
    Panorama mode requires quite some memory, especially after the frame
    num bump done with commit aa0733567c30 ("Make panorama able to go 270
    degrees in landscape"). Some devices run out of memory while taking a
    panorama picture and force close Snap before the picture is complete.
    We have a config to reduce the memory requirements that reduces the
    size of each frame, but it's applied only if ro.config.low_ram is
    true. Bump the default value to 100 and always respect it. Devices
    having ro.config.low_ram set to true will have to override this
    config from their device tree.
    
    BUGBASH-326
    
    Change-Id: Ic6d24b17b2293adf8d715904c8c1874a4c624e99

--- src/com/android/camera/WideAnglePanoramaModule.java
+++ src/com/android/camera/WideAnglePanoramaModule.java
@@ -16,7 +16,6 @@
 
 package com.android.camera;
 
-import android.app.ActivityManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
@@ -917,12 +916,7 @@ public class WideAnglePanoramaModule
             return;
         }
 
-        int perct = 100;
-        final ActivityManager am = (ActivityManager)
-                mActivity.getSystemService(Context.ACTIVITY_SERVICE);
-        if (am.isLowRamDevice()) {
-            perct = mActivity.getResources().getInteger(R.integer.panorama_frame_size_reduction);
-        }
+        int perct = mActivity.getResources().getInteger(R.integer.panorama_frame_size_reduction);
 
         int width = (mCameraPreviewWidth * perct) / 100;
         int height = (mCameraPreviewHeight * perct) / 100;
