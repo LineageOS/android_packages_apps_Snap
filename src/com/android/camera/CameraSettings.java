d1f15ea33201d98ae56ee9385edcfb535d4ad59a
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-26 20:50:22 +0200

    Snap: add option to set number of supported cameras
    OnePlus 5 has 3 physical cameras but reports 4 available cameras.
    
    The first two cameras are regularly used, the other two cameras are for Bokeh mode and other
    zooming effects and only internally used by OnePlus.
    
    This change allows us to hide the additional cameras from users.
    
    Change-Id: Ida299bd19862d32dc078d18c261c50500242e9ff

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -297,6 +297,7 @@ public class CameraSettings {
     private final Parameters mParameters;
     private final CameraHolder.CameraInfo[] mCameraInfo;
     private final int mCameraId;
+    private final int mNumberOfSupportedCameras;
 
     public static String mKeyIso = null;
     public static String mKeyIsoValues = null;
@@ -479,6 +480,10 @@ public class CameraSettings {
 
         // Bokeh mode
         mSupportBokehMode = mContext.getResources().getBoolean(R.bool.support_bokeh_mode);
+
+        // Number of supported cameras
+        mNumberOfSupportedCameras = mContext.getResources().getInteger(
+                R.integer.number_of_supported_cameras);
     }
 
     public PreferenceGroup getPreferenceGroup(int preferenceRes) {
@@ -1322,6 +1327,10 @@ public class CameraSettings {
             if (numOfCameras > 2) {
                 numOfCameras = 2;
             }
+        }
+
+        if (mNumberOfSupportedCameras != -1) {
+            numOfCameras = mNumberOfSupportedCameras;
         }
 
         CharSequence[] entryValues = new CharSequence[numOfCameras];
