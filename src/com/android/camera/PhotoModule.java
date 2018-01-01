3e3cc18dd56c4b795f1cd1476e51a38ab1ea9ef3
Paul Keith <javelinanddart@gmail.com>
2018-01-01 18:56:04 +0100

    Revert "Snap: Bring back original shutter icons and animation"
    No reason to do use caf ones
    
    This reverts commit a465e3a0cf7a81c7cb9a0e80cbfa7527e7d7de77.
    
    Change-Id: I611351ddfa0b24087cc58466dcba95ab4999d1bd

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1145,8 +1145,6 @@ public class PhotoModule
                     return;
                 }
 
-                mUI.doShutterAnimation();
-
                 Location loc = getLocationAccordPictureFormat(mParameters.get(KEY_PICTURE_FORMAT));
 
                 mLongShotCaptureCount++;
