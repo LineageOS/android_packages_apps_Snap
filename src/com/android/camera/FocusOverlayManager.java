ce91805e98d5afec76ac2165880280406e164e7a
Ethan Chen <intervigil@gmail.com>
2018-03-16 23:57:14 -0700

    Snap: Configure focus ring preview dimensions
    Change-Id: I49d7f28fb2f731e18c73b9d944882f9f8a4fb5c2

--- src/com/android/camera/FocusOverlayManager.java
+++ src/com/android/camera/FocusOverlayManager.java
@@ -180,6 +180,7 @@ public class FocusOverlayManager {
     public void setPreviewRect(Rect previewRect) {
         if (!mPreviewRect.equals(previewRect)) {
             mPreviewRect.set(previewRect);
+            mFocusRing.configurePreviewDimensions(CameraUtil.rectToRectF(mPreviewRect));
             resetCoordinateTransformer();
             mInitialized = true;
         }
