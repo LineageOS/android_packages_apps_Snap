4ae205146682f417cd3b537862dcd92ab714db50
Ethan Chen <intervigil@gmail.com>
2018-03-16 23:57:14 -0700

    Snap: Configure focus ring preview dimensions
    Change-Id: I49d7f28fb2f731e18c73b9d944882f9f8a4fb5c2

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2828,7 +2828,7 @@ public class PhotoModule
                 if (mFocusManager == null) {
                     mFocusManager = new FocusOverlayManager(mPreferences, defaultFocusModes,
                             mInitialParams, this, mMirror,
-                            mActivity.getMainLooper(), mUI != null ? mUI.getFocusRing() : null, mActivity);
+                            mActivity.getMainLooper(), mUI.getFocusRing(), mActivity);
                 }
             }
         }
