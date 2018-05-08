316cfa3c4a8ce59b138cee4ec0bfe8c82ebd8af0
Santhosh Kumar Thimmanna Bhattar <sthim@codeaurora.org>
2018-05-08 10:59:13 +0530

    SnapdragonCamera: Fix compilation issues for AOSP upgrade
    Fixed compilation issues for AOSP upgrade.
    
    Change-Id: I10db85d552a6aa45798e3f4b98ff8516c5d8b0ab

--- jni/feature_mos/src/mosaic/Blend.cpp
+++ jni/feature_mos/src/mosaic/Blend.cpp
@@ -1329,7 +1329,7 @@ void Blend::ComputeBlendParameters(MosaicFrame **frames, int frames_size, int is
         double dy = prevY - firstY;
 
         // If the mosaic was captured by sweeping horizontally
-        if (abs(lxpos - fxpos) > abs(lypos - fypos))
+        if (fabs(lxpos - fxpos) > fabs(lypos - fypos))
         {
             m_wb.horizontal = 1;
             // Calculate radius position to make ends exactly the same Y offset
