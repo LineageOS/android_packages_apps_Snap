9c8110fa914119eb5d6ee87167a84cb16b394bc1
zhuw <zhuw@codeaurora.org>
2018-08-07 10:38:26 +0800

    Add new Layout for FR 52032
    add new layout in camera app main page.
    The new layout is camera module switcher.
    
    Change-Id: I2c05809b81e7f82d7dabc40233c7e8ac905ef6c4

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += xmp_toolkit
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -15,6 +16,7 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src_wrapper)
 LOCAL_SRC_FILES += $(call all-renderscript-files-under, rs)
 
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
 
 include $(LOCAL_PATH)/version.mk
 LOCAL_AAPT_FLAGS := \
