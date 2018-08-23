d9ebd65bb924a9d635df14416b4967ea5ec1f6ba
xianming wang <mingwax@codeaurora.org>
2018-08-23 16:42:17 +0800

    SnapdragonCamera: Adapter HFR/HSR 60 for single camera
    Adapter HFR/HSR 60fps for single camera.
    
    CRs-Fixed: 2282590
    Change-Id: I2fa1851d5aebe6b225a03fd5923efdeec76f9c69

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -352,7 +352,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
         mDependendsOnMap = new HashMap<>();
         mFilteredKeys = new HashSet<>();
         try {
-            if (cameraId < mCharacteristics.size() -1) {
+            if (mCharacteristics.size() > 0) {
                 mExtendedHFRSize = mCharacteristics.get(cameraId).get(CaptureModule.hfrFpsTable);
             }
         }catch(IllegalArgumentException exception) {
