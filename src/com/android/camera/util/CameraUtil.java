4b2fabda85bf4de68a50b06cb617720deee36b4d
xianming wang <mingwax@codeaurora.org>
2018-09-05 11:12:49 +0800

    SnapdragonCamera: Fix parameters NullPointerException
    Fix parameters is NullPointerException in China App Stress.
    
    CRs-Fixed: 2307806
    Change-Id: I71fff4cb32361ad0e66d6460ca8dc01167c8a009

--- src/com/android/camera/util/CameraUtil.java
+++ src/com/android/camera/util/CameraUtil.java
@@ -188,6 +188,7 @@ public class CameraUtil {
     }
 
     public static boolean isVideoSnapshotSupported(Parameters params) {
+        if (params == null) return false;
         return TRUE.equals(params.get(VIDEO_SNAPSHOT_SUPPORTED));
     }
 
