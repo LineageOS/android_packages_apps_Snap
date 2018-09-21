db77993f39061896a7114439d377f3dd95f95875
jinwu <jinwu@codeaurora.org>
2018-09-21 18:06:11 +0800

    SnapdragonCamera:Fix NP
    Catch NullPointerException in setEndOfStream
    to avoid FC when session is null
    
    Change-Id: Ic238e6bd0a2e894515374d3df9298d5f28aa3cda
    CRs-Fixed: 2287143

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -4670,6 +4670,8 @@ public class CaptureModule implements CameraModule, PhotoController,
             e.printStackTrace();
         } catch (IllegalStateException e) {
             e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
     }
 
