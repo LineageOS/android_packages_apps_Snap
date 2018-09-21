08de5f445f89615e5d91b3586568a879d6e2439f
jinwu <jinwu@codeaurora.org>
2018-09-21 18:03:54 +0800

    SnapdragonCamera:Fix CalledFromWrongThreadException
    make showUIafterRecording run on main thread to
    avoid CalledFromWrongThreadException
    
    Change-Id: Ib26100e9ed148a5b924bcb5894f0be07f00fafba
    CRs-Fixed: 2287143

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -4330,7 +4330,12 @@ public class CaptureModule implements CameraModule, PhotoController,
 
     public void startMediaRecording() {
         if (!startMediaRecorder()) {
-            mUI.showUIafterRecording();
+            mActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mUI.showUIafterRecording();
+                }
+            });
             releaseMediaRecorder();
             mFrameProcessor.setVideoOutputSurface(null);
             restartSession(true);
