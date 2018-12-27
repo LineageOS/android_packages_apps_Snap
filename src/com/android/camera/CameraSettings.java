c8c7085a9dc8ecafadf51f87f562eaf2e4f88257
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-12-27 12:29:15 +0100

    Snap: make support for bokeh mode configurable per device
    Support is disabled by default.
    
    Change-Id: I5ab480e4eed9d314ae6f5ef78c5246397ebbe587

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -301,6 +301,8 @@ public class CameraSettings {
     public static String mKeyIso = null;
     public static String mKeyIsoValues = null;
 
+    private static boolean mSupportBokehMode = false;
+
     private static final HashMap<Integer, String>
             VIDEO_ENCODER_TABLE = new HashMap<Integer, String>();
     public static final HashMap<String, Integer>
@@ -470,6 +472,9 @@ public class CameraSettings {
         } else {
             Log.d(TAG, "Using key for iso-values: " + mKeyIsoValues);
         }
+
+        // Bokeh mode
+        mSupportBokehMode = mContext.getResources().getBoolean(R.bool.support_bokeh_mode);
     }
 
     public PreferenceGroup getPreferenceGroup(int preferenceRes) {
@@ -1307,9 +1312,11 @@ public class CameraSettings {
             return;
         }
 
-//        if (numOfCameras > 2 ) {
-//            numOfCameras = 2;
-//        }
+        if (!mSupportBokehMode) {
+            if (numOfCameras > 2) {
+                numOfCameras = 2;
+            }
+        }
 
         CharSequence[] entryValues = new CharSequence[numOfCameras];
         for (int i = 0; i < numOfCameras; ++i) {
@@ -1654,6 +1661,11 @@ public class CameraSettings {
 
     public static boolean isBokehModeSupported(Parameters params) {
         boolean ret = false;
+
+        if (!mSupportBokehMode) {
+            return ret;
+        }
+
         if (null != params) {
             String val = params.get(KEY_QC_IS_BOKEH_MODE_SUPPORTED);
             if ("1".equals(val)) {
@@ -1665,6 +1677,11 @@ public class CameraSettings {
 
     public static boolean isBokehMPOSupported(Parameters params) {
         boolean ret = false;
+
+        if (!mSupportBokehMode) {
+            return ret;
+        }
+
         if (null != params) {
             String val = params.get(KEY_QC_IS_BOKEH_MPO_SUPPORTED);
             if ("1".equals(val)) {
