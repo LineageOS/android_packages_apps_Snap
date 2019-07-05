228fc421e590785ae5deac4cb24fbecf0ee37e19
Han Wang <416810799@qq.com>
2019-07-05 19:01:42 +0200

    CameraButtonIntentReceiver: Initialize CameraHolder before use
     * Mimics the logic in DisableCameraReceiver
    
    Change-Id: If5fea25ec22e30cd42d74d6ea150d3d06d80a7c7

--- src/com/android/camera/CameraButtonIntentReceiver.java
+++ src/com/android/camera/CameraButtonIntentReceiver.java
@@ -19,6 +19,11 @@ package com.android.camera;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.android.camera.util.CameraUtil;
+
+import org.codeaurora.snapcam.R;
 
 /**
  * {@code CameraButtonIntentReceiver} is invoked when the camera button is
@@ -31,9 +36,22 @@ import android.content.Intent;
  * activity.
  */
 public class CameraButtonIntentReceiver extends BroadcastReceiver {
+    private static final String TAG = "CameraButtonIntentReceiver";
+    private boolean mCamera2supported = false;
+    private boolean mCamera2enabled = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Check if the device supports Camera API 2
+        mCamera2supported = CameraUtil.isCamera2Supported(context);
+        Log.d(TAG, "Camera API 2 supported: " + mCamera2supported);
+
+        mCamera2enabled = mCamera2supported &&
+                context.getResources().getBoolean(R.bool.support_camera_api_v2);
+        Log.d(TAG, "Camera API 2 enabled: " + mCamera2enabled);
+
+        CameraHolder.setCamera2Mode(context, mCamera2enabled);
+
         // Try to get the camera hardware
         CameraHolder holder = CameraHolder.instance();
         ComboPreferences pref = new ComboPreferences(context);
