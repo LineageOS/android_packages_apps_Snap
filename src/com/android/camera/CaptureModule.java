74122ce821056553f91ed39c8d6597e284041a23
jiaoyuan <jiaoyuan@codeaurora.org>
2019-08-30 12:30:05 +0800

    SnapdraongCamera: update zoom value
    update zoomseekbar and zoomrender value
    according to the zoom setting value
    
    Change-Id: Id8f51398813d0e63a8cec9837fe6c7d7c636b560

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -4987,6 +4987,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         int zoom = Integer.parseInt(zoomStr);
         if ( zoom !=0 ) {
             mZoomValue = (float)zoom;
+            mUI.updateZoomSeekBar(mZoomValue);
         }else{
             mZoomValue = 1.0f;
         }
