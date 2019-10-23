a490b5455fb98581d6bb3519271ac253055b2191
LuK1337 <priv.luk@gmail.com>
2019-10-23 20:47:31 +0200

    Fix NPE when removing RAW setting
    * That preference is already gone when developer
      options are disabled.
    
    Change-Id: I040d153561cce691e8e5689bb810585071cb5836

--- src/com/android/camera/SettingsActivity.java
+++ src/com/android/camera/SettingsActivity.java
@@ -727,7 +727,7 @@ public class SettingsActivity extends PreferenceActivity {
         int cameraId = mSettingsManager.getCurrentCameraId();
         Size[] rawSize = mSettingsManager.getSupportedOutputSize(cameraId,
                 ImageFormat.RAW10);
-        if (rawSize == null) {
+        if (mDeveloperMenuEnabled && rawSize == null) {
             Preference p = findPreference(SettingsManager.KEY_SAVERAW);
             p.setEnabled(false);
         }
