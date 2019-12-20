81444a1683c159568ae18d0c4dc0598b068a6de2
jiaoyuan <jiaoyuan@codeaurora.org>
2019-12-20 16:04:01 +0800

    check Selfie Flash before continuous shot
    check Selfie Flash before continuous shot
    
    Change-Id: I9eb00d9b353eed9c74bde6ac093bda6f02521ed5

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -6514,7 +6514,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             Log.d(TAG, "Start Longshot");
             mLongshotActive = true;
             mUI.enableVideo(!mLongshotActive);
-            takePicture();
+            checkSelfieFlashAndTakePicture();
         } else {
             RotateTextToast.makeText(mActivity, "Long shot not support", Toast.LENGTH_SHORT).show();
         }
