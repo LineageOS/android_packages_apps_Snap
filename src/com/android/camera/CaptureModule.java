ae7449acf9b8f61e5e75e8445c31a0ac8c1b19a4
jiaoyuan <jiaoyuan@codeaurora.org>
2020-01-13 15:18:45 +0800

    cancel count down if has it
    cancel and do not send new if already has
    count down.
    
    Change-Id: I53bc60849035bfdd9d299a12276e0f7cfe54c969

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -6442,6 +6442,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         // finished. If not, cancel the previous countdown and start a new one.
         if (mUI.isCountingDown()) {
             mUI.cancelCountDown();
+            return;
         }
         if (seconds > 0) {
             mUI.startCountDown(seconds, true);
