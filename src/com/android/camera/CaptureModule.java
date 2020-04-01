5a4dd11513bb7e73db78d4c5f1209549a47d60b7
xianming wang <mingwax@codeaurora.org>
2020-04-01 15:40:20 +0800

    SnapdragonCamera: Close ImageReader after stopBackgroundThread
    Close ImageReader after stoprBackgroundThread.
    
    CRs-Fixed: 2645642
    Change-Id: If1f8658f7f1b7f1676301bcdc71383be844ede10

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -3437,9 +3437,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                                     NamedEntity name = mNamedImages.getNextNameEntity();
                                     String title = (name == null) ? null : name.title;
                                     long date = (name == null) ? -1 : name.date;
-
                                     byte[] bytes = getJpegData(image);
-
                                     Log.i(TAG, "image format:" + image.getFormat());
                                     if (image.getFormat() == ImageFormat.RAW10) {
                                         mActivity.getMediaSaveService().addRawImage(bytes, title,
@@ -3457,9 +3455,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                                         } else {
                                             orientation = CameraUtil.getJpegRotation(getMainCameraId(),mOrientation);
                                         }
-
-
-
                                         if (mIntentMode != CaptureModule.INTENT_MODE_NORMAL) {
                                             mJpegImageData = bytes;
                                             if (!mQuickCapture) {
@@ -3778,6 +3773,19 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
     }
 
+    private void closeImageReader() {
+        for (int i = MAX_NUM_CAM - 1; i >= 0; i--) {
+            if (null != mImageReader[i]) {
+                mImageReader[i].close();
+                mImageReader[i] = null;
+            }
+        }
+        if (null != mVideoSnapshotImageReader) {
+            mVideoSnapshotImageReader.close();
+            mVideoSnapshotImageReader = null;
+        }
+    }
+
     /**
      * Closes the current {@link CameraDevice}.
      */
@@ -3819,11 +3827,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                     mCameraOpened[i] = false;
                     mCaptureSession[i] = null;
                 }
-
-                if (null != mImageReader[i]) {
-                    mImageReader[i].close();
-                    mImageReader[i] = null;
-                }
             }
 
             mIsLinked = false;
@@ -3831,11 +3834,6 @@ public class CaptureModule implements CameraModule, PhotoController,
             if (null != mMediaRecorder) {
                 mMediaRecorder.release();
                 mMediaRecorder = null;
-            }
-
-            if (null != mVideoSnapshotImageReader) {
-                mVideoSnapshotImageReader.close();
-                mVideoSnapshotImageReader = null;
             }
         } catch (InterruptedException e) {
             mCameraOpenCloseLock.release();
@@ -4155,6 +4153,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mActivity.setResultEx(Activity.RESULT_CANCELED, new Intent());
             mActivity.finish();
         }
+        closeImageReader();
         mJpegImageData = null;
     }
 
