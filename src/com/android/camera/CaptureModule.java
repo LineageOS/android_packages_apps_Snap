ZVNexus/android_packages_apps_ParanoidCamera

Snapcam: unlock expouse when change mode
unlock expouse when change mode

Change-Id: I318b8891f512d21188502e22efcec29c5fce531a

src/com/android/camera/CaptureModule.java
@@ -4318,7 +4318,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             }
         } else {
             if (mState[getMainCameraId()] == STATE_WAITING_TOUCH_FOCUS ||
-                    mState[getMainCameraId()] == STATE_PREVIEW) {
+                    mState[getMainCameraId()] == STATE_PREVIEW || mLockAFAE) {
                 cancelTouchFocus(getMainCameraId());
             }
         }
@@ -4522,6 +4522,9 @@ public class CaptureModule implements CameraModule, PhotoController,
         mOringalCameraId = CURRENT_ID;
         if(mCurrentSceneMode.mode == CameraMode.VIDEO){
             enableVideoButton(false);//disable the video button before media recorder is ready
+        }
+        if (mCurrentSceneMode.mode != CameraMode.HFR){
+            mHighSpeedCapture = false;
         }
         checkRTBCameraId();
         if (!isBackCamera() && !frontIsAllowed()) {
@@ -8479,7 +8482,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         Log.d(TAG, "restart all, old id:" + mOringalCameraId + ",newId:" + newId);
         if(mLockAFAE) {
             mLockAFAE = false;
-            unlockFocus(mCurrentSceneMode.getCurrentId());
+            applySettingsForUnlockExposure(mPreviewRequestBuilder[mOringalCameraId], mOringalCameraId);
             updateLockAFAEVisibility();
         }
         if(mOringalCameraId == newId){

LineageOS/android_packages_apps_Snap

src/com/android/camera/CaptureModule.java
@@ -4281,7 +4281,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             }
         } else {
             if (mState[getMainCameraId()] == STATE_WAITING_TOUCH_FOCUS ||
-                    mState[getMainCameraId()] == STATE_PREVIEW) {
+                    mState[getMainCameraId()] == STATE_PREVIEW || mLockAFAE) {
                 cancelTouchFocus(getMainCameraId());
             }
         }
@@ -4483,6 +4483,9 @@ public class CaptureModule implements CameraModule, PhotoController,
                 + (resumeFromRestartAll ? " isResumeFromRestartAll" : ""));
         if(mCurrentSceneMode.mode == CameraMode.VIDEO){
             enableVideoButton(false);//disable the video button before media recorder is ready
+        }
+        if (mCurrentSceneMode.mode != CameraMode.HFR){
+            mHighSpeedCapture = false;
         }
         checkRTBCameraId();
         if (!isBackCamera() && !frontIsAllowed()) {
@@ -8311,7 +8314,7 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
         if(mLockAFAE) {
             mLockAFAE = false;
-            unlockFocus(mCurrentSceneMode.getCurrentId());
+            applySettingsForUnlockExposure(mPreviewRequestBuilder[CURRENT_ID], CURRENT_ID);
             updateLockAFAEVisibility();
         }
         onPauseBeforeSuper();