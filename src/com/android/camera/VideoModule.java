507c9c5ed5df45e23d64003ba6d553113c161a14
Michael W <baddaemon87@gmail.com>
2020-10-29 22:18:54 +0100

    Snap: Remove saving to sdcard
    * Doesn't work anymore since the introduction of scoped storage
    * Giving the users this option doesn't make sense anymore
    
    -> Not available >> Doesn't work
    
    Change-Id: I763ff23b04cd093f15f439a91160c2b086b18f72

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -111,8 +111,6 @@ public class VideoModule implements CameraModule,
 
     private static final int SCREEN_DELAY = 2 * 60 * 1000;
 
-    private static final int SDCARD_SIZE_LIMIT = 4000 * 1024 * 1024;
-
     private static final long SHUTTER_BUTTON_TIMEOUT = 0L; // 0ms
 
     /**
@@ -140,7 +138,6 @@ public class VideoModule implements CameraModule,
 
     private ComboPreferences mPreferences;
     private PreferenceGroup mPreferenceGroup;
-    private boolean mSaveToSDCard = false;
 
     // Preference must be read before starting preview. We check this before starting
     // preview.
@@ -547,9 +544,6 @@ public class VideoModule implements CameraModule,
 
         mContentResolver = mActivity.getContentResolver();
 
-        Storage.setSaveSDCard(
-            mPreferences.getString(CameraSettings.KEY_CAMERA_SAVEPATH, "0").equals("1"));
-        mSaveToSDCard = Storage.isSaveSDCard();
         // Surface texture is from camera screen nail and startPreview needs it.
         // This must be done before startPreview.
         mIsVideoCaptureIntent = isVideoCaptureIntent();
@@ -1227,13 +1221,6 @@ public class VideoModule implements CameraModule,
     }
 
     @Override
-    public void onSwitchSavePath() {
-        mUI.setPreference(CameraSettings.KEY_CAMERA_SAVEPATH, "1");
-        RotateTextToast.makeText(mActivity, R.string.on_switch_save_path_to_sdcard,
-                Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
     public void installIntentFilter() {
         if(mReceiver != null)
             return;
@@ -1843,10 +1830,6 @@ public class VideoModule implements CameraModule,
             maxFileSize = requestedSizeLimit;
         }
 
-        if (Storage.isSaveSDCard() && maxFileSize > SDCARD_SIZE_LIMIT) {
-            maxFileSize = SDCARD_SIZE_LIMIT;
-        }
-
         try {
             mMediaRecorder.setMaxFileSize(maxFileSize);
         } catch (RuntimeException exception) {
@@ -1914,12 +1897,7 @@ public class VideoModule implements CameraModule,
         // Used when emailing.
         String filename = title + convertOutputFormatToFileExt(outputFileFormat);
         String mime = convertOutputFormatToMimeType(outputFileFormat);
-        String path = null;
-        if (Storage.isSaveSDCard() && SDCard.instance().isWriteable()) {
-            path = SDCard.instance().getDirectory() + '/' + filename;
-        } else {
-            path = Storage.DIRECTORY + '/' + filename;
-        }
+        String path = Storage.DIRECTORY + '/' + filename;
         mCurrentVideoValues = new ContentValues(9);
         mCurrentVideoValues.put(Video.Media.TITLE, title);
         mCurrentVideoValues.put(Video.Media.DISPLAY_NAME, filename);
@@ -1997,7 +1975,7 @@ public class VideoModule implements CameraModule,
         stopVideoRecording();
         mUI.showUIafterRecording();
         if (what == MediaRecorder.MEDIA_RECORDER_ERROR_UNKNOWN) {
-            // We may have run out of space on the sdcard.
+            // We may have run out of storage space.
             mActivity.updateStorageSpaceAndHint();
         }
     }
@@ -3117,8 +3095,6 @@ public class VideoModule implements CameraModule,
                 setCameraParameters(false);
             }
             mRestartPreview = false;
-            Storage.setSaveSDCard(
-                mPreferences.getString(CameraSettings.KEY_CAMERA_SAVEPATH, "0").equals("1"));
             mActivity.updateStorageSpaceAndHint();
             mActivity.initPowerShutter(mPreferences);
             mActivity.initMaxBrightness(mPreferences);
--- src/com/android/camera/util/PersistUtil.java
+++ src/com/android/camera/util/PersistUtil.java
@@ -81,8 +81,6 @@ public class PersistUtil {
             SystemProperties.getInt("persist.sys.camera.perf.memlimit", 120);
     private static final boolean PERSIST_CAMERA_UI_AUTO_TEST_ENABLED =
             SystemProperties.getBoolean("persist.sys.camera.ui.auto_test", false);
-    private static final boolean PERSIST_CAMERA_SAVE_IN_SD_ENABLED =
-            SystemProperties.getBoolean("persist.sys.env.camera.saveinsd", false);
     private static final boolean PERSIST_LONG_SAVE_ENABLED =
             SystemProperties.getBoolean("persist.sys.camera.longshot.save", false);
     private static final boolean PERSIST_CAMERA_PREVIEW_RESTART_ENABLED =
@@ -229,10 +227,6 @@ public class PersistUtil {
 
     public static boolean isAutoTestEnabled(){
         return PERSIST_CAMERA_UI_AUTO_TEST_ENABLED;
-    }
-
-    public static boolean isSaveInSdEnabled(){
-        return PERSIST_CAMERA_SAVE_IN_SD_ENABLED;
     }
 
     public static boolean isLongSaveEnabled(){
