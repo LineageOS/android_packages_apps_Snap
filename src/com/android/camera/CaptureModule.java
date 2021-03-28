eda3e6567fa732a9a90b497ce1dcb148f298323b
Timi <timi.rautamaki@gmail.com>
2021-03-28 21:47:43 +0200

    Snap: reorganize api2 layout
     * added icons from https://material.io/resources/icons/
       except ic_switch_{hfr,sat} are self made
    
    Change-Id: I9ffc276915297f0cbb31258d325b2459bf20dc1e

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -30,6 +30,7 @@ import android.content.SharedPreferences;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
@@ -4944,6 +4945,9 @@ public class CaptureModule implements CameraModule, PhotoController,
             return;
         }
         Log.d(TAG, "onSingleTapUp " + x + " " + y);
+
+        mUI.closeModeSwitcher(true);
+
         int currentId = mCurrentSceneMode.getCurrentId();
         if(mLockAFAE) {
             mLockAFAE = false;
@@ -6813,6 +6817,8 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
 
         Log.d(TAG,"onShutterButtonClick");
+
+        mUI.closeModeSwitcher(true);
 
         if (mCurrentSceneMode.mode == CameraMode.HFR ||
                 mCurrentSceneMode.mode == CameraMode.VIDEO) {
@@ -8993,6 +8999,8 @@ public class CaptureModule implements CameraModule, PhotoController,
         updateZoomSeekBarVisible();
         mUI.updateZoomSeekBar(1.0f);
         updateZoom();
+        mUI.closeModeSwitcher(true);
+        mUI.setCurrentModeIcon(mode);
         return 1;
     }
 
@@ -9028,6 +9036,16 @@ public class CaptureModule implements CameraModule, PhotoController,
 
     public String[] getSelectableModes() {
         return mSelectableModes;
+    }
+
+    public List<Integer> getCameraModeIconList() {
+        ArrayList<Integer> cameraModeIcons = new ArrayList<>();
+        TypedArray ic = mActivity.getResources()
+                .obtainTypedArray(R.array.camera_modes_back);
+        for (SceneModule sceneModule : mSceneCameraIds) {
+            cameraModeIcons.add(ic.getResourceId(sceneModule.mode.ordinal(), 0));
+        }
+        return cameraModeIcons;
     }
 
     private class SceneModule {
