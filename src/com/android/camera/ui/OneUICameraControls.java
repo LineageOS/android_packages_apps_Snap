4f8951b2bb71d9eece565b2e5200e550d5b76c25
Timi <timi.rautamaki@gmail.com>
2021-03-28 21:47:43 +0200

    Snap: reorganize api2 layout
    Change-Id: I9ffc276915297f0cbb31258d325b2459bf20dc1e

--- src/com/android/camera/ui/OneUICameraControls.java
+++ src/com/android/camera/ui/OneUICameraControls.java
@@ -27,13 +27,18 @@ import android.graphics.Path;
 import android.graphics.Point;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Display;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import android.support.v7.widget.RecyclerView;
 
 import com.android.camera.CaptureModule;
 import com.android.camera.Storage;
@@ -41,11 +46,12 @@ import com.android.camera.imageprocessor.filter.BeautificationFilter;
 
 import org.codeaurora.snapcam.R;
 
-public class OneUICameraControls extends RotatableLayout {
+public class OneUICameraControls extends RotatableLayout
+        implements View.OnClickListener {
 
     private static final String TAG = "CAM_Controls";
 
-    private static final float TOP_PANEL_SPACE_NUM = 4f;
+    private static final float TOP_PANEL_SPACE_NUM = 5f;
     private static final float BOTTOM_PANEL_SPACE_NUM = 5f;
     private static final float PANEL_INDEX_0 = 0f;
     private static final float PANEL_INDEX_1 = 1f;
@@ -68,8 +74,11 @@ public class OneUICameraControls extends RotatableLayout {
     private View mMakeupSeekBarHighText;
     private View mMakeupSeekBarLayout;
     private View mCancelButton;
+    private View mModeSwitcher;
     private ViewGroup mProModeLayout;
     private View mSettingsButton;
+
+    private RecyclerView mModeSelectLayout;
 
     private ArrowTextView mRefocusToast;
 
@@ -110,10 +119,25 @@ public class OneUICameraControls extends RotatableLayout {
     private RotateLayout mIsoRotateLayout;
     private RotateLayout mZoomSeekBarLayout;
 
+    private static final int[] mDrawIds = {
+            R.drawable.ic_switch_camera,
+            R.drawable.ic_switch_video,
+            R.drawable.ic_switch_pan,
+            R.drawable.ic_cam_switcher_qr,
+            R.drawable.ic_switch_photosphere,
+            R.drawable.ic_switch_gcam,
+    };
+
+    private boolean mShowingPopup;
+
+    Context mContext;
+
     public OneUICameraControls(Context context, AttributeSet attrs) {
         super(context, attrs);
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         setWillNotDraw(false);
+
+        mContext = context;
 
         mRefocusToast = new ArrowTextView(context);
         addView(mRefocusToast);
@@ -167,6 +191,7 @@ public class OneUICameraControls extends RotatableLayout {
         mRemainingPhotos = (LinearLayout) findViewById(R.id.remaining_photos);
         mRemainingPhotosText = (TextView) findViewById(R.id.remaining_photos_text);
         mCancelButton = findViewById(R.id.cancel_button);
+        mModeSwitcher = findViewById(R.id.mode_switcher);
         mProModeLayout = (ViewGroup) findViewById(R.id.pro_mode_layout);
 
         mExposureText = (TextView) findViewById(R.id.exposure_value);
@@ -183,9 +208,27 @@ public class OneUICameraControls extends RotatableLayout {
         mIsoRotateLayout = (RotateLayout) findViewById(R.id.iso_rotate_layout);
         mZoomSeekBarLayout = (RotateLayout) findViewById(R.id.zoom_seekbar_layout);
 
+        mModeSelectLayout = (RecyclerView) findViewById(R.id.mode_select_layout);
+        mModeSelectLayout.setVisibility(View.GONE);
+
+        mFlashButton.setOnClickListener(this);
+        mPauseButton.setOnClickListener(this);
+        mFrontBackSwitcher.setOnClickListener(this);
+        mMakeupSeekBar.setOnClickListener(this);
+        mSceneModeSwitcher.setOnClickListener(this);
+        mFilterModeSwitcher.setOnClickListener(this);
+
+        mModeSwitcher.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showModeSwitcher();
+            }
+       });
+
         mExposureText.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeModeSwitcher();
                 resetProModeIcons();
                 int mode = mProMode.getMode();
                 if (mode == ProMode.EXPOSURE_MODE) {
@@ -199,6 +242,7 @@ public class OneUICameraControls extends RotatableLayout {
         mManualText.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeModeSwitcher();
                 resetProModeIcons();
                 int mode = mProMode.getMode();
                 if (mode == ProMode.MANUAL_MODE) {
@@ -212,6 +256,7 @@ public class OneUICameraControls extends RotatableLayout {
         mWhiteBalanceText.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeModeSwitcher();
                 resetProModeIcons();
                 int mode = mProMode.getMode();
                 if (mode == ProMode.WHITE_BALANCE_MODE) {
@@ -225,6 +270,7 @@ public class OneUICameraControls extends RotatableLayout {
         mIsoText.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeModeSwitcher();
                 resetProModeIcons();
                 int mode = mProMode.getMode();
                 if (mode == ProMode.ISO_MODE) {
@@ -238,6 +284,7 @@ public class OneUICameraControls extends RotatableLayout {
         mZoomSeekbarText.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                closeModeSwitcher();
                 resetProModeIcons();
                 int mode = mProMode.getMode();
                 if (mode == ProMode.ZOOM_MODE) {
@@ -250,8 +297,8 @@ public class OneUICameraControls extends RotatableLayout {
         });
         mViews = new View[]{
                 mSceneModeSwitcher, mFilterModeSwitcher, mFrontBackSwitcher,
-                mFlashButton,
-                mPreview, mPauseButton, mCancelButton, mSettingsButton
+                mFlashButton, mPreview, mPauseButton, mCancelButton,
+                mSettingsButton, mModeSwitcher
         };
         mBottomLargeSize = getResources().getDimensionPixelSize(
                 R.dimen.one_ui_bottom_large);
@@ -320,6 +367,18 @@ public class OneUICameraControls extends RotatableLayout {
         }
     }
 
+    public void showModeSwitcher() {
+        mModeSelectLayout.setX(mModeSwitcher.getX() - mModeSelectLayout.getWidth() / 4);
+        mModeSelectLayout.setY(mModeSwitcher.getY() - mModeSelectLayout.getHeight()
+                + mModeSwitcher.getHeight());
+        mModeSelectLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void closeModeSwitcher() {
+        if (mModeSelectLayout != null)
+            mModeSelectLayout.setVisibility(View.GONE);
+    }
+
     private void setLocation(View v, boolean top, float idx) {
         if(v == null) {
             return;
@@ -356,6 +415,7 @@ public class OneUICameraControls extends RotatableLayout {
         int rotation = getUnifiedRotation();
         setLocation(mSceneModeSwitcher, true, PANEL_INDEX_0);
         setLocation(mFilterModeSwitcher, true, PANEL_INDEX_1);
+        setLocation(mModeSwitcher, false, 4f);
         if (mIsVideoMode) {
             setLocation(mMute, true, PANEL_INDEX_1);
             setLocation(mFlashButton, true, PANEL_INDEX_2);
@@ -365,9 +425,9 @@ public class OneUICameraControls extends RotatableLayout {
             setLocation(mVideoShutter, false, PANEL_INDEX_2);
             setLocation(mExitBestPhotpMode ,false, PANEL_INDEX_4);
         } else {
-            setLocation(mFlashButton, true, PANEL_INDEX_2);
-            setLocation(mSettingsButton,true, PANEL_INDEX_3);
-            setLocation(mFrontBackSwitcher, false, 3.15f);
+            setLocation(mFlashButton, true, PANEL_INDEX_3);
+            setLocation(mSettingsButton,true, PANEL_INDEX_4);
+            setLocation(mFrontBackSwitcher, true, PANEL_INDEX_2);
             if (mIntentMode == CaptureModule.INTENT_MODE_CAPTURE) {
                 setLocation(mShutter, false, PANEL_INDEX_2);
                 setLocation(mCancelButton, false, 0.85f);
@@ -532,6 +592,14 @@ public class OneUICameraControls extends RotatableLayout {
         mIsoRotateLayout.setOrientation(orientation, animation);
         mZoomSeekBarLayout.setOrientation(orientation, animation);
         mProMode.setOrientation(orientation);
+
+        // Reorient the camera modes
+        for (int i = 0; i < ((ViewGroup) mModeSelectLayout).getChildCount(); i++) {
+            ViewGroup v = (ViewGroup) mModeSelectLayout.getChildAt(i);
+            Rotatable r = (Rotatable) v.getChildAt(0);
+            r.setOrientation(orientation, animation);
+        }
+
         layoutRemaingPhotos();
     }
 
@@ -634,5 +702,10 @@ public class OneUICameraControls extends RotatableLayout {
             case ProMode.ZOOM_MODE:
                 mZoomSeekbarText.setText(value);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        closeModeSwitcher();
     }
 }
